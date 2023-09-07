eebcd94b86f48d3477893a2f83af0b0837c4d57d
ade.hafis <ade.hafis@socialbread.id>
2023-09-07 11:46:42 +0700

    update

---
+++ ProjectGUI/src/main/java/com/hafisrabbani/projectgui/ProjectGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hafisrabbani.projectgui;
+
+/**
+ *
+ * @author hafis
+ */
+public class ProjectGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
